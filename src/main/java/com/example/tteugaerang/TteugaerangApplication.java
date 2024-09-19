6c53f3ba0fcde4f152b2789ae923f55d7d6b4a15
ghdgu <ghdguswl13@gmail.com>
2024-09-19 21:52:58 +0900

    Initial commit

---
+++ src/main/java/com/example/tteugaerang/TteugaerangApplication.java
@@ -0,0 +1,13 @@
+package com.example.tteugaerang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TteugaerangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TteugaerangApplication.class, args);
+	}
+
+}
