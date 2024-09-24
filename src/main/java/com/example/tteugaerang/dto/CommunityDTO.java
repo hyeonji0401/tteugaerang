7921daeb165f7e248163097605919a87546603c7
ghdgu <ghdguswl13@gmail.com>
2024-09-24 21:52:08 +0900

    feat : 커뮤니티 dto

---
+++ src/main/java/com/example/tteugaerang/dto/CommunityDTO.java
@@ -0,0 +1,16 @@
+package com.example.tteugaerang.dto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommunityDTO {
+    private String writer;
+    private String title;
+    private String content;
+    private String writeTime;
+}
+
+
