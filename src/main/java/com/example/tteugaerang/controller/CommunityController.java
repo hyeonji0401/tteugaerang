025bad938b1fd8cce7780be33b369c69377ac576
hyeonji0401 <hhj@smail.kongju.ac.kr>
2024-09-29 23:31:30 +0900

    feat : 게시글 삭제 컨트롤러

--- src/main/java/com/example/tteugaerang/controller/CommunityController.java
+++ src/main/java/com/example/tteugaerang/controller/CommunityController.java
@@ -1,9 +1,20 @@
 package com.example.tteugaerang.controller;
 
+import com.example.tteugaerang.service.CommunityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class CommunityController {
+    private final CommunityService communityService;
+
+    @GetMapping("/communitydelete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        communityService.delete(id);
+        return "redirect:/community";
+    }
+
 }
